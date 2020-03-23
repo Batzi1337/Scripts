f38989921c025d586f413b0d27f83802030a1686
Batzi1337 <marco.batzinger@gmx.de>
2020-03-23 08:50:10 +0000

    added click.py
    python script which clicks every 2 sec at point (10,10) on the screen

---
+++ click.py
@@ -0,0 +1,10 @@
+import win32api, win32con, time
+def click(x,y):
+    win32api.SetCursorPos((x,y))
+    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN,x,y,0,0)
+    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP,x,y,0,0)
+
+if __name__ == "__main__":
+    while (True):
+        click(10,10)
+        time.sleep(120)
